fddf32f4e79d8cf45f44fb1e4c29c2ac1c985d7e
Assaf Shomer <assafshomer@gmail.com>
2014-01-02 11:23:25 +0200

    Fix test

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,12 +4,12 @@ class AddressesController < ApplicationController
   
   def private
   	@title=private_title
-		@key = Bitcoin::generate_key
-		@address = Bitcoin::pubkey_to_address(@key[1])  	
-		@qr = RQRCode::QRCode.new( @address, :size => 8, :level => :h )		
-
+		# @key = Bitcoin::generate_key
+		# @address = Bitcoin::pubkey_to_address(@key[1])  	
+		# @qr = RQRCode::QRCode.new( @address, :size => 8, :level => :h )		
 		@howmany=cookies[:howmany].to_i
 		@addresses=generate_addresses_array(@howmany)				
+		# send_data(render_to_string, :filename => "object.html", :type => "text/html")
   end
 
   def public
