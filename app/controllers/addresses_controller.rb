a78e085a1e2a8a870244a2d8e97326756b964cd7
Assaf Shomer <assafshomer@gmail.com>
2014-01-03 12:34:29 +0200

    Added public page. For now just a copy of the priate page. currently vies are calling the address generator, this is bad design. It makes the tests slow and also I cannot navigate back and forth and keep the same address around. The setup page should be responsible for generation when i click the generate button. Also, I should prevent navigating to the private/public pages when there are no addresses around and redirect to setup

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -15,5 +15,11 @@ class AddressesController < ApplicationController
 
   def public
   	@title=public_title
+		@howmany=cookies[:howmany].to_i
+		@howmany||=1
+		redirect_to root_path unless @howmany > 0
+		@addresses=generate_addresses_array(@howmany)
+		@download=params[:download]				
+		send_data(render_to_string, :filename => "coldstorage.html") if @download  	
   end
 end
