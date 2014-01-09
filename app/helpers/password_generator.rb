2b31f2eb60fd58224e19982afae0ee92cd8553c5
Assaf Shomer <assafshomer@gmail.com>
2014-01-09 11:03:38 +0200

    password generator working

--- app/helpers/password_generator.rb
+++ app/helpers/password_generator.rb
@@ -1,25 +1,30 @@
 class PasswordGenerator
 
-	attr_reader :password, :entropy
+	attr_reader :password
 
-	# def initialize(length=30)
-	# 	@password=generate_strong_password(length)
-	# 	# @entropy=Math.log(length*alphabet.length,2)
-	# end
+	def initialize(length=30)
+		@password=generate_strong_password(length)
+	end
 
 	def self.alphabet
 		meta_array.flatten.join('')
 	end
 
+	def self.calculate_entropy(string)
+		return -1 unless self.in_alphabet?(string)
+		string.length * Math.log(self.alphabet.length,2)
+	end
 
-
+	def entropy
+		PasswordGenerator.calculate_entropy(@password)
+	end
 
 	private
 
 	def generate_strong_password(howlong=30)
 		result=""
 		while result.length < howlong
-			kind=meta_array.sample						
+			kind=PasswordGenerator.meta_array.sample						
 			temp=kind.sample
 			result<<temp if result.index(temp).nil? && kind.index(result[-1]).nil?
 		end
--- spec/helpers/password_generator_spec.rb
+++ spec/helpers/password_generator_spec.rb
@@ -2,14 +2,19 @@ require 'spec_helper'
 
 describe "password generator" do
 	let!(:pass) { PasswordGenerator.new }
+	let!(:alphabet) { PasswordGenerator.alphabet }
+	let!(:eunit) { Math.log(alphabet.length,2) }
 	subject { pass }
 	describe "attributes" do
 		it { should respond_to :password }
 		it { should respond_to :entropy }
 	end
 
-	describe "alphabet" do
-		let!(:alphabet) { PasswordGenerator.alphabet }
+	describe "initializer" do
+		its(:entropy) { should == 30 * eunit }
+	end
+
+	describe "alphabet" do		
 		subject { alphabet }
 		its(:class) { should == String }
 		its(:length) { should == 72 }
@@ -34,5 +39,11 @@ describe "password generator" do
 		specify {PasswordGenerator.in_alphabet?('a(4!@#$').should be_false}
 	end
 	
+	describe "calculate entropy" do
+		specify {PasswordGenerator.calculate_entropy('').should == 0}
+		specify {PasswordGenerator.calculate_entropy('a').should == eunit }
+		specify {PasswordGenerator.calculate_entropy(' ').should == -1 }
+		specify {PasswordGenerator.calculate_entropy('abc').should == 3*eunit }
+	end
 
 end
