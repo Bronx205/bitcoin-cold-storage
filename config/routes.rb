55cfdafe210134f605de0e733aa5cb1aeb3075b5
Assaf Shomer <assafshomer@gmail.com>
2014-01-12 14:42:29 +0200

    Created ovens resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Coldcoin::Application.routes.draw do
 
- 	resources :freezers, only: [:new, :create, :show, :load, :show_loaded]
+ 	resources :freezers, only: [:new, :create, :show]
+ 	resources :ovens, only: [:new, :create, :show]
  
  	root to: "static#home"
  
   match "/freeze",  	to: 'freezers#new',   				via: :get
   match "/cold_view", to: 'freezers#show',  				via: :get
-  match "/heatup",  	to: 'freezers#load',   			via: :get
-  match "/hot_view",  to: 'freezers#show_loaded',  via: :get
+  match "/heatup",  	to: 'ovens#new',   						via: :get
+  match "/hot_view",  to: 'ovens#show',						  via: :get
 
   match "/help",    	to: 'static#help',    				via: :get
   match "/about",   	to: 'static#about',	  				via: :get
