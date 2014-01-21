8fb1ab0cb2790a6dc680a9c4073b634f1b95be4a
Assaf Shomer <assafshomer@gmail.com>
2014-01-21 10:09:31 +0200

    More qm testing

--- spec/helpers/quartermaster_spec.rb
+++ spec/helpers/quartermaster_spec.rb
@@ -22,7 +22,16 @@ describe "quartermaster" do
 			it { expect{qm.save_public_addresses}.not_to raise_error }
 			before { qm.save_public_addresses }
 			describe "should save a csv file named addresses_list to a public folder under the files dir" do				
-
+				specify{File.exist?(path).should be_true }
+				describe "with a list of valid addresses" do
+					let!(:file) { CSV.read(path) }
+					subject { file }
+					its(:length) { should == size+1 }
+					it { file[0][0].should == '#' }
+					it { file[0][1].should == 'Bitcoin Address' }
+					it { file[1][0].should == '1' }
+					it { Bitcoin::valid_address?(file[1][1]).should be_true}
+				end	
 			end
 		end
 	end
