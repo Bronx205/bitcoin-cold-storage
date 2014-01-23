27ecf56759e15fe04cc872b16a5547ab4a008fc7
Assaf Shomer <assafshomer@gmail.com>
2014-01-23 10:29:21 +0200

    one more test

--- spec/requests/freezers_integration_spec.rb
+++ spec/requests/freezers_integration_spec.rb
@@ -3,7 +3,7 @@ require 'shared_examples'
 include ViewsHelper
 include FilesHelper
 
-describe "Freezers Slow specs:" do
+describe "Freezers" do
 	subject { page }
 	before do
 	  visit freeze_path
@@ -28,5 +28,20 @@ describe "Freezers Slow specs:" do
 		end		
 		it_should_behave_like 'the private keys page'		
 		it { should have_selector('div.alert.alert-password', text: 'randomly generated') }
+		describe "cold storage files are saved and are fresh" do
+			let!(:pa_path) { public_addresses_file_path('csv') }
+			let!(:pk_path) { private_keys_file_path('csv',false) }
+			specify{File.exist?(pa_path).should be_true }			
+			specify{(File.ctime(pa_path).to_f-Time.now.to_f).to_i.should be < 1}
+			specify{File.exist?(pk_path).should be_true }
+			specify{(File.ctime(pk_path).to_f-Time.now.to_f).to_i.should be < 1}					
+		end		
 	end
+	describe "should not die on a big dispatch" do
+		before do
+			fill_in 'howmany', with: 10		  
+		  click_button generate_button			  
+		end		
+		it_should_behave_like 'the private keys page'	
+	end		
 end
