b460f062e4fea5e41971feebfb9baa9b9054da70
Assaf Shomer <assafshomer@gmail.com>
2014-01-26 01:49:05 +0200

    Uploaded file finally read into create controller. Now need to build the logic and vies properly

--- app/controllers/inspectors_controller.rb
+++ app/controllers/inspectors_controller.rb
@@ -4,11 +4,20 @@ class InspectorsController < ApplicationController
   end
 
   def create
+  	@csv_file=params[:csv].read
+  	
   end
 
   def addresses
+  	redirect_to root_path unless File.exist?(public_addresses_file_path('csv'))
+  	@title=addresses_title
+    @data=CSV.read(public_addresses_file_path('csv'))
+    @keys=build_addresses_hash_array(@data)
   end
 
   def private_keys
+  	@title=private_keys_title
+    @data=CSV.read(private_keys_file_path('csv',false))
+    @keys=build_private_keys_hash_array(@data)
   end
 end
