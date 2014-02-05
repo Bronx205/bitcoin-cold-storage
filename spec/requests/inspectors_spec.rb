7aff874ebde2c32261646faeaf01a2f9eb3e07c4
Assaf Shomer <assafshomer@gmail.com>
2014-02-05 09:29:53 +0200

    Added spec for inspecting an encrypted private keys file witout providing a password, instead providing enough ssss shares. Also some simplification to the way inspectors spec is written. Instead of looking at the unencrypted csv for the test, use the value hardcoded in the test, this way we are not relying on an extra step of loading a file

--- spec/requests/inspectors_spec.rb
+++ spec/requests/inspectors_spec.rb
@@ -4,9 +4,7 @@ require 'shared_examples'
 include ViewsHelper
 include FilesHelper
 
-describe "Inspectors:" do
-	let!(:test_pa_path) { file_fixtures_directory+'valid/10_addresses.csv' }
-	let!(:test_unencrypted_pk_path) { file_fixtures_directory+'valid/10_private_keys.csv' }	
+describe "Inspectors:" do	
 	let!(:test_encrypted_pk_path) { file_fixtures_directory+'valid/2_private_keys_moohaha.csv.aes' }	
 	let!(:test_decrypted_pk_path) { file_fixtures_directory+'valid/2_private_keys_moohaha.csv' }	
 	subject { page }
@@ -22,7 +20,7 @@ describe "Inspectors:" do
 		it { should have_selector('div.alert.alert-error', text: no_file_loaded_flash)}
 	end
 	describe "loading an addresses csv file" do
-		let!(:data) { CSV.read(test_pa_path) }
+		let!(:test_pa_path) { file_fixtures_directory+'valid/10_addresses.csv' }
 		before do
 		  attach_file "file", test_pa_path
 		  click_button recover_button
@@ -31,8 +29,8 @@ describe "Inspectors:" do
 		it { should_not have_selector('td.black')}
 		it_should_behave_like 'it does not have download buttons'
 		describe "and show the addresses correctly" do
-			it { should have_selector('td.text_pubkey#address_1', text: data[1][1]) }
-			it { should have_selector('td.text_pubkey#address_10', text: data[10][1]) }
+			it { should have_selector('td.text_pubkey#address_1', text: '1PUBCSPrspWYz6FdJ1CX3SFoQg96Zr62RS') }
+			it { should have_selector('td.text_pubkey#address_10', text: '1CXaVvECDcvBb8PFMJAsCPGhqkKwP1tD8K') }
 			it { should_not have_selector('td.text_pubkey#address_11') }
 			describe "clicking the QR link should generate the QR" do
 				before { click_link 'address_qr_btn_1' }
@@ -41,7 +39,7 @@ describe "Inspectors:" do
 		end
 	end
 	describe "loading an unencrypted private keys csv file" do
-		let!(:data) { CSV.read(test_unencrypted_pk_path) }
+		let!(:test_unencrypted_pk_path) { file_fixtures_directory+'valid/10_private_keys.csv' }	
 		before do
 		  attach_file "file", test_unencrypted_pk_path
 		  click_button recover_button
@@ -50,8 +48,8 @@ describe "Inspectors:" do
 		it { should_not have_selector('td.black')}
 		it_should_behave_like 'it does not have download buttons'		
 		describe "should show the keys correctly" do
-			it { should have_selector('td.text_pubkey#address_1', text: data[1][1]) }
-			it { should have_selector('td.text_pubkey#address_10', text: data[10][1]) }
+			it { should have_selector('td.text_pubkey#address_1', text: '15CzKMKshYnsS3w1ujED3WSQ1T38nAs3CC') }
+			it { should have_selector('td.text_prvkey#prvkey_wif_10', text: '5KCG1j27PTVojXXZLf389rKr9PmgiE64NyDQbxFCx1V3c6qtUK4') }
 			it { should have_selector("td#qr_address_2") }
 			it { should have_selector("td#qr_prvkey_wif_2") }				
 			it { should_not have_selector('td.text_pubkey#address_11') }
@@ -65,8 +63,7 @@ describe "Inspectors:" do
 			end						
 		end
 	end
-	describe "loading an encrypted private keys csv.aes file" do
-		let!(:data) { CSV.read(test_decrypted_pk_path) }
+	describe "loading an encrypted private keys csv.aes file with a password" do
 		before do
 		  attach_file "file", test_encrypted_pk_path
 		  fill_in 'password', with: 'moohaha'
@@ -75,13 +72,30 @@ describe "Inspectors:" do
 		it_should_behave_like 'the private keys page'
 		it_should_behave_like 'it does not have download buttons'		
 		describe "should show the keys correctly" do
-			it { should have_selector('td.text_pubkey#address_1', text: data[1][1]) }
-			it { should have_selector('td.text_pubkey#address_2', text: data[2][1]) }
+			it { should have_selector('td.text_pubkey#address_1', text: '1K17kr8ZCNBEGjZ7gJDV52SiZy9uJYH58U') }
+			it { should have_selector('td.text_prvkey#prvkey_wif_2', text: '5Jr89tkiXSU6SxQhX8CA9tfUET5wUwVvcAfxfSz8Snm3uEjSSGL') }
 			it { should have_selector("td#qr_address_2") }
 			it { should have_selector("td#qr_prvkey_wif_2") }				
 			it { should_not have_selector('td.text_pubkey#address_11') }						
 		end
-	end	
+	end
+	describe "loading an encrypted private keys csv.aes file with 2 ssss shares" do
+		let!(:encrypted_pkss_path) { file_fixtures_directory+'valid/private_keys_foo.csv.aes' }	
+		before do
+		  attach_file "file", encrypted_pkss_path
+		  fill_in 'shares', with: '0013B27E42CDD7BC70A'+"\n"+'0025F013B5A3328040A'
+		  click_button recover_button
+		end
+		it_should_behave_like 'the private keys page'
+		it_should_behave_like 'it does not have download buttons'		
+		describe "should show the keys correctly" do
+			it { should have_selector('td.text_pubkey#address_1', text: '1HKoy9Gp823n7157Hxj8RvKMLV5njpUEU9') }
+			it { should have_selector('td.text_prvkey#prvkey_wif_3', text: '5JLtMz4FB3NPRBbsf75QLYeBDHUL4A6nC8g5BjPuBYe2tNfj3iX') }
+			it { should have_selector("td#qr_address_2") }
+			it { should have_selector("td#qr_prvkey_wif_2") }				
+			it { should_not have_selector('td.text_pubkey#address_4') }						
+		end
+	end		
 	describe "invalid files" do
 		describe "foo.bar" do		
 			before do
