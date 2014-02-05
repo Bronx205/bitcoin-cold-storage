2f20660c3b38bf79a65ce2883117df84361f778f
Assaf Shomer <assafshomer@gmail.com>
2014-02-05 11:49:08 +0200

    Drying up spec

--- spec/helpers/password_splitter_spec.rb
+++ spec/helpers/password_splitter_spec.rb
@@ -52,32 +52,29 @@ describe "password splitter" do
 			let!(:pass) { "I love yoO $$ a Bu$hel!! \n and \t peck...." }
 			let!(:ps) { PasswordSplitter.new(5,3,pass) }
 			let!(:ps_missing_shares) { ps.shares.drop(3).join("\n") }
-			let!(:ps_shares_1) { ps.shares.drop(2).join("\n") }
-			let!(:ps_shares_2) { ps.shares.rotate.drop(2).join("\n") }
-			let!(:ps_shares_3) { ps.shares.rotate(2).drop(2).join("\n") }
 			it "should raise error if not enough shares" do
 				expect {empty.join(3,ps_missing_shares)}.to raise_error('Not enough shares')
 			end
-			it "should retrieve the password in various share combos" do	
-				empty.join(3,ps_shares_1).should == pass
-				empty.join(3,ps_shares_2).should == pass
-				empty.join(3,ps_shares_3).should == pass
-			end				
+			3.times do |x|
+				it "should retrieve the password in various share combos" do	
+					empty.join(3,ps.shares.rotate(x).drop(2).join("\n")).should == pass
+				end						
+			end		
 		end
 		describe "white space characters in input string" do
 			let(:empty) { PasswordSplitter.new }
 			let!(:pass) { "I love yoO $$ a Bu$hel!! \n and \t peck...." }
 			let!(:ps) { PasswordSplitter.new(5,3,pass) }
-			let!(:ps_shares_rn) { ps.shares.drop(2).join("\r\n") }
-			let!(:ps_shares_t) { ps.shares.drop(2).join("\t") }
-			let!(:ps_shares_s) { ps.shares.drop(2).join(" ") }
-			let!(:ps_shares_ns) { ps.shares.drop(2).join("\n ") }			
-			it "should retrieve the password" do	
-				empty.join(3,ps_shares_rn).should == pass
-				empty.join(3,ps_shares_t).should == pass
-				empty.join(3,ps_shares_s).should == pass
-				empty.join(3,ps_shares_ns).should == pass			
-			end				
+			["\r\n","\r","\n","\t"," ","\n ","\r\n\t"].each do |example|
+				it "should retrieve the password" do	
+					empty.join(3,ps.shares.drop(2).join(example)).should == pass
+				end					
+			end
+			['','x',','].each do |example|
+				it "should not retrieve the password" do	
+					expect{empty.join(3,ps.shares.drop(2).join(example))}.to raise_error(RuntimeError,'Not enough shares')
+				end					
+			end								
 		end
 	end
 end
