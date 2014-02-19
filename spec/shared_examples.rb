a8f45d32dc1130219b516ef8b71d32b3fa8b8bfb
Assaf Shomer <assafshomer@gmail.com>
2014-02-19 09:45:17 +0200

    Changed default number of addresses to generate to 10

--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -11,7 +11,7 @@ shared_examples_for "the freeze page" do
 	it { should have_title full_title(freeze_page_title) }
 	it { should have_selector('h2', freeze_page_header)}
 	it { should have_selector('input#howmany.input-mini') }
-	it { should have_xpath("//input[@value=0]")}
+	it { should have_xpath("//input[@value=10]")}
 	it { should have_button generate_button}
 	it { should have_selector('input#password.input-xxlarge') }
 	it { should have_selector('input#ssss_n.input-mini') }
