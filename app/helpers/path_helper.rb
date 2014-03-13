45543784c044265786e200c6056035ddad15a3fe
Assaf Shomer <assafshomer@gmail.com>
2014-03-13 11:03:23 +0200

    PathHelper now savvy to usb path

--- app/helpers/path_helper.rb
+++ app/helpers/path_helper.rb
@@ -1,39 +1,43 @@
 module PathHelper
 
 	def relative_root_path
-		File.expand_path(Rails.root)
+		File.expand_path(Rails.root) +'/'
 	end
 
 	def usb_path
 		'/media/coldstorage/'
 	end
 
-	def coldstorage_directory
-		relative_root_path +  '/files/'
+	def coldstorage_directory(usb=false)
+		if usb
+			return usb_path
+		else
+			return relative_root_path +  'files/' 				
+		end		
 	end
 
 	def file_fixtures_directory
-		relative_root_path +  '/spec/fixtures/files/'
+		relative_root_path +  'spec/fixtures/files/'
 	end
 
 	def encrypted_file_name
 		'coldstorage.html.aes'
 	end
 
-	def public_directory_path
-		coldstorage_directory + 'public/'
+	def public_directory_path(usb=false)
+		coldstorage_directory(usb) + 'public/'
 	end
 			
-	def private_directory_path
-		coldstorage_directory + 'PRIVATE/'
+	def private_directory_path(usb=false)
+		coldstorage_directory(usb) + 'PRIVATE/'
 	end
 
-	def encrypted_directory_path
-		private_directory_path + 'encrypted/'
+	def encrypted_directory_path(usb=false)
+		private_directory_path(usb) + 'encrypted/'
 	end
 
-	def unencrypted_directory_path
-		private_directory_path + 'NON-ENCRYPTED/'
+	def unencrypted_directory_path(usb=false)
+		private_directory_path(usb) + 'NON-ENCRYPTED/'
 	end	
 
 	def public_addresses_file_name
@@ -46,20 +50,20 @@ module PathHelper
 		'password_share'
 	end
 
-	def public_addresses_file_path(file_type)
-		public_directory_path+public_addresses_file_name+$tag+"."+file_type
+	def public_addresses_file_path(file_type,usb=false)
+		public_directory_path(usb)+public_addresses_file_name+$tag+"."+file_type
 	end
 
-	def private_keys_file_path(file_type,encrypted = true)
+	def private_keys_file_path(file_type,encrypted,usb=false)
 		if encrypted
-			return encrypted_directory_path+private_keys_file_name+$tag+"."+file_type+".aes"		
+			return encrypted_directory_path(usb)+private_keys_file_name+$tag+"."+file_type+".aes"		
 		else
-			return unencrypted_directory_path+private_keys_file_name+$tag+"."+file_type
+			return unencrypted_directory_path(usb)+private_keys_file_name+$tag+"."+file_type
 		end		
 	end
 
-	def password_shares_path(number)
+	def password_shares_path(number,usb=false)
 		raise 'Share number must be positive' unless number > 0
-		encrypted_directory_path+password_share_file_name+'_'+number.to_s+$tag+'.csv'
+		encrypted_directory_path(usb)+password_share_file_name+'_'+number.to_s+$tag+'.csv'
 	end
 end
--- spec/helpers/path_helper_spec.rb
+++ spec/helpers/path_helper_spec.rb
@@ -4,21 +4,45 @@ describe PathHelper do
 
 	describe "path selection" do
 		it "should choose the right path for unencrypted" do
-			private_keys_file_path('foo',false).should == unencrypted_directory_path + private_keys_file_name + '.foo'
+			private_keys_file_path('foo',false).should == unencrypted_directory_path + private_keys_file_name + $tag + '.foo'
 		end
 		it "should choose the right path for encrypted" do
-			private_keys_file_path('foo').should == encrypted_directory_path + private_keys_file_name + '.foo.aes'
+			private_keys_file_path('foo',true).should == encrypted_directory_path + private_keys_file_name + $tag + '.foo.aes'
 		end		
 	end
 	describe "usb vs internal path selection" do
 		it "should work for public dir" do			
-			public_directory_path.should == relative_root_path +  '/files/public/'
+			public_directory_path.should == relative_root_path +  'files/public/'
 			public_directory_path(true).should == usb_path +  'public/'
 		end
 		it "should work for public file path" do			
-			public_addresses_file_path('csv').should == relative_root_path +  '/files/public/' +public_addresses_file_name+'.csv'
-			public_directory_path(true).should == usb_path +  'public/'
-		end		
+			public_addresses_file_path('csv').should == relative_root_path +  'files/public/' +public_addresses_file_name + $tag + '.csv'
+			public_addresses_file_path('csv',true).should == usb_path +  'public/' + public_addresses_file_name + $tag + '.csv'
+		end
+		it "should work for PRIVATE dir" do			
+			private_directory_path.should == relative_root_path +  'files/PRIVATE/'
+			private_directory_path(true).should == usb_path +  'PRIVATE/'
+		end
+		it "should work for encrypted dir" do			
+			encrypted_directory_path.should == relative_root_path +  'files/PRIVATE/encrypted/'
+			encrypted_directory_path(true).should == usb_path +  'PRIVATE/encrypted/'
+		end
+		it "should work for NON-ENCRYPTED dir" do			
+			unencrypted_directory_path.should == relative_root_path +  'files/PRIVATE/NON-ENCRYPTED/'
+			unencrypted_directory_path(true).should == usb_path +  'PRIVATE/NON-ENCRYPTED/'
+		end
+		it "should work for unencrypted private file path" do			
+			private_keys_file_path('csv',false).should == relative_root_path +  'files/PRIVATE/NON-ENCRYPTED/' +private_keys_file_name + $tag + '.csv'
+			private_keys_file_path('csv',false,true).should == usb_path +  'PRIVATE/NON-ENCRYPTED/' + private_keys_file_name + $tag + '.csv'
+		end
+		it "should work for encrypted private file path" do			
+			private_keys_file_path('csv',true).should == relative_root_path +  'files/PRIVATE/encrypted/' +private_keys_file_name + $tag + '.csv.aes'
+			private_keys_file_path('csv',true,true).should == usb_path +  'PRIVATE/encrypted/' + private_keys_file_name + $tag + '.csv.aes'
+		end
+		it "should work for password shares file path" do			
+			password_shares_path(1).should == relative_root_path +  'files/PRIVATE/encrypted/' +password_share_file_name+'_1'+ $tag + '.csv'
+			password_shares_path(1,true).should == usb_path +  'PRIVATE/encrypted/' + password_share_file_name + '_1' + $tag + '.csv'
+		end													
 	end
 
 end
